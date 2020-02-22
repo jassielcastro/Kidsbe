9f3ca9526b8cc28b50d011970eb21f0e4e0b98b7
Alan Jassiel Castro <alan.castro>
2020-02-21 18:37:52 -0600

    Transition between fragments

---
+++ app/src/main/java/com/ajcm/kidstube/extensions/NavigationExt.kt
@@ -0,0 +1,12 @@
+package com.ajcm.kidstube.extensions
+
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.findNavController
+
+fun Fragment.navigateTo(@IdRes idAction: Int, bundle: Bundle? = null, navOptions: NavOptions? = null, extras: Navigator.Extras? = null) {
+    findNavController().navigate(idAction, bundle, navOptions, extras)
+}
