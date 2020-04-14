08e0cb3236bb65e749bfe309bdba895b00f60438
Alan Jassiel Castro <alan.castro>
2020-04-14 18:11:11 -0500

    AppCompatDelegate added

--- app/src/main/java/com/ajcm/kidstube/KidsTubeApp.kt
+++ app/src/main/java/com/ajcm/kidstube/KidsTubeApp.kt
@@ -1,12 +1,14 @@
 package com.ajcm.kidstube
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
 class KidsTubeApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
         initDI()
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
     }
 
 }
